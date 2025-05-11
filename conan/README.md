3cac92a0399aa6523d7a489377c9f2a50d881f5f
Manuel Moos <z-man@users.sf.net>
2025-05-11 23:03:41 +0200

    Update conanfile to trunk dependencies
    Sadly no ftgl available on the main hub.

--- conan/README.md
+++ conan/README.md
@@ -1,9 +1,12 @@
 # Conan
 
-If you wish to use conan versions of some of our dependences
-(libxml2 and curl at the moment), invoke
+If you wish to use conan versions of some of our dependences, invoke
 
-`conan build ./conan/conanfile.py --build=missing`
+`conan build conan --build=missing`
 
 before calling the configure script.
+
+To update the lockfile, use
+
+`conan lock create conan`
 
--- conan/conanfile.py
+++ conan/conanfile.py
@@ -13,9 +13,30 @@ class Pkg(ConanFile):
 
     requires = \
             "libcurl/[>=7]", \
+            "sdl_ttf/[>2.0]", \
+            "sdl_image/[>2.0]", \
+            "sdl_mixer/[>2.0]", \
+            "glew/[>=2.2 <3.0]", \
+            "protobuf/[>=2.4 <3.22]", \
+            "freetype/[>2.0]", \
             "libxml2/[>=2.9.10]"
 
     default_options = {
+        "sdl/*:x11": False,
+        "sdl/*:pulseaudio": False,
+        "sdl_mixer/*:flac": False,
+        "sdl_mixer/*:opus": False,
+        "sdl_mixer/*:modplug": False,
+        "sdl_mixer/*:tinymidi": False,
+        "sdl_image/*:xv": False,
+        "sdl_image/*:lbm": False,
+        "sdl_image/*:qoi": False,
+        "sdl_image/*:svg": False,
+        "sdl_image/*:tga": False,
+        "sdl_image/*:xcf": False,
+        "sdl_image/*:xpm": False,
+        "sdl_image/*:with_libtiff": False,
+        "sdl_image/*:with_libwebp": False,
         "libcurl/*:with_ssl": False,
         "libcurl/*:with_https": False,
         "libcurl/*:with_ftp": False,
@@ -40,7 +61,13 @@ class Pkg(ConanFile):
 
     keep_imports = True
 
+    def requirements(self):
+        # force resolving conflict between mixer and image
+        self.requires("sdl/2.30.8", force=True)
+        pass
+
     def generate(self):
+
         # copy libraries
         libs_path = os.path.join(self.build_folder, "lib")
         for dep_name, dep in self.dependencies.items():
