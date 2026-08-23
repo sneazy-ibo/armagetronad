1fff54061f99518b5d61899986f5193143619fcc
Manuel Moos <z-man@users.sf.net>
2026-08-23 23:06:54 +0200

    Integrate doctest
    single header unit test framework

---
+++ src/test/update_doctest.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Download the latest version of the doctest header.
+# Doctest is a single header testing framework and this is the easiest way to integrate it.
+# Doctest itself is distributed under the MIT license, that still applies if you extract the copy
+# found here.
+
+wget -O doctest.h https://raw.githubusercontent.com/doctest/doctest/master/doctest/doctest.h
