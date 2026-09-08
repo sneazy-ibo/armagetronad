a10dcc883c2ed6a945d10dce07fb9739c44f2e31
Manuel Moos <z-man@users.sf.net>
2026-09-08 20:47:55 +0000

    Add include guard

--- src/engine/eAxis.h
+++ src/engine/eAxis.h
@@ -1,3 +1,6 @@
+#ifndef ArmageTron_AXIS_H
+#define ArmageTron_AXIS_H
+
 #include "defs.h"
 
 #include <math.h>
@@ -41,3 +44,4 @@ protected:
     void SnapWinding(); //!< push axes that are really close to coordinate directions to them
 };
 
+#endif
