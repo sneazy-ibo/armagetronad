06a97d529558e0607175ca8c1b15642d574dbc36
Manuel Moos <z-man@users.sf.net>
2026-09-08 20:48:29 +0000

    Fix chat filter test compilation the hacky way
    Just force linking in the .o files that are reported missing.

--- src/test/chat_prefix_test.cpp
+++ src/test/chat_prefix_test.cpp
@@ -4,6 +4,16 @@
 #include "eChat.h"
 #include "tString.h"
 #include "ePlayer.h"
+#include "eGrid.h"
+
+#undef BAD_CAST
+#define BAD_CAST(x) reinterpret_cast<xmlChar const *>(x)
+
+// weird that these are required, I guess I do not understand the linker.
+#include "eAxis.h"
+static eAxis se_justToWorkAroundLinkerErrors1;
+#include "ePath.h"
+static ePath se_justToWorkAroundLinkerErrors2;
 
 struct Stats
 {
@@ -81,7 +91,7 @@ void ProcessNode( xmlTextReaderPtr reader )
     
     int type = xmlTextReaderNodeType( reader );
     
-    if ( type == XML_READER_TYPE_END_ELEMENT && xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST "Session" ) )
+    if ( type == XML_READER_TYPE_END_ELEMENT && xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST("Session") ) )
     {
         TestSession( currentSession );
     }
@@ -89,19 +99,19 @@ void ProcessNode( xmlTextReaderPtr reader )
     if ( type != XML_READER_TYPE_ELEMENT )
         return;
     
-    if ( xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST "Session" ) )
+    if ( xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST("Session") ) )
     {
         currentSession = Session( xmlTextReaderGetParserLineNumber( reader ) );
     }
-    else if ( xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST "Player" ) )
+    else if ( xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST("Player") ) )
     {
         xmlChar *xmlPlayer = xmlTextReaderReadString( reader );
         currentSession.player_ = ConvertXMLString( xmlPlayer );
         xmlFree( xmlPlayer );
     }
-    else if ( xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST "Said" ) )
+    else if ( xmlStrEqual( xmlTextReaderConstName( reader ), BAD_CAST("Said") ) )
     {
-        xmlChar *xmlTime = xmlTextReaderGetAttribute( reader, BAD_CAST "time" );
+        xmlChar *xmlTime = xmlTextReaderGetAttribute( reader, BAD_CAST("time") );
         xmlChar *xmlSaid = xmlTextReaderReadString( reader );
         
         long time = atol( (const char *)xmlTime );
