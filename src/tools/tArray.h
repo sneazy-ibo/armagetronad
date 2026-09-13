9b0f4f3e423f7b0a7d9b5618910f772d08e8905c
Manuel Moos <z-man@users.sf.net>
2026-09-13 19:11:00 +0000

    Restore missing include

--- src/tools/tArray.h
+++ src/tools/tArray.h
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 // #include "defs.h"
 #include <vector>
+#include <tuple> // for std::ignore
 #include "tError.h"
 // #include "tSafePTR.h"
 
