50b5b12ef1e1f0a9a6772bf59fd07337db785d24
Manuel Moos <z-man@users.sf.net>
2026-09-15 20:41:07 +0000

    Must not rely on CHECK

--- src/test/CodingStyle.h
+++ src/test/CodingStyle.h
@@ -91,7 +91,12 @@ public:
     static int GetNumberOfObjects() noexcept { return s_numberOfObjects_.GetCount(); }
 
     // FYI Rule of Three: Implement destructor, copy constructor and assignment operator together
-    virtual ~cReferenceCounted() noexcept { CHECK(s_numberOfObjects_.TryCountDown()); } // FYI if this is a leaf class, mark it with 'final', then you can make the destructor non-virtual
+    virtual ~cReferenceCounted() noexcept
+    {
+        auto success = s_numberOfObjects_.TryCountDown();
+        tASSERT(success);
+    } // FYI if this is a leaf class, mark it with 'final', then you can make the destructor non-virtual
+
     cReferenceCounted(cReferenceCounted const& that) noexcept
         : tReferencable<cReferenceCounted>(that)
     {
