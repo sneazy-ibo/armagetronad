8fda5b6629589b4b43dc762da8f3cbddefe92a7e
Manuel Moos <z-man@users.sf.net>
2026-09-17 21:20:37 +0000

    Add basic eTeam test

--- src/test/eTeam_test.cpp
+++ src/test/eTeam_test.cpp
@@ -1,44 +1,32 @@
 #include "doctest.h"
 #include "eTeam.h"
 
+#include "MockConsole.h"
+
 // Tests for eTeam system
 // Purpose: Document the status quo behavior and detect regressions
 
-#if false
 TEST_SUITE("eTeam")
 {
-    TEST_CASE("eTeam base class")
+    TEST_CASE("eTeam base tests")
     {
-        GIVEN("eTeam base class")
+        MockConsole mockConsole; // swallow log output
+
+        GIVEN("eTeam")
         {
-            // Note: eTeam may have dependencies that make it difficult
-            // to test in isolation. For now, we'll test only that the class exists.
-            
-            THEN("eTeam class exists")
+            auto team = tRefPtr<eTeam>::Make();
+
+            THEN("it can take up one ePlayerNetID")
             {
-                // We can't easily create a eTeam without initialization,
-                // but we can verify the class exists
-                CHECK(sizeof(eTeam) > 0);
+                auto player = tRefPtr<ePlayerNetID>::Make();
+                player->SetName("Z");
+                team->AddPlayer(player);
+
+                AND_THEN("it takes the player's name")
+                {
+                    CHECK(team->Name().StartsWith("Z"));
+                }
             }
         }
     }
 }
-#endif
-
-// TODO: More comprehensive eTeam tests could be added, but the system
-// has significant dependencies on the engine and player systems that make
-// isolated unit testing challenging. The current tests focus on:
-// 1. Base class existence
-//
-// Additional tests that could be added:
-// 1. Team creation and destruction - requires engine initialization
-// 2. Team member management - requires player objects
-// 3. Team scoring and statistics - requires game logic
-// 4. Team color management - requires rendering
-// 5. Team communication - requires network
-//
-// These would need to be integration tests rather than unit tests, or would
-// require significant refactoring to make the team system more testable.
-
-// Z-Man: Seems to me like one should be able to test eTeam and ePlayer together,
-// at least do some basic list management.
