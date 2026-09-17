d4a76bc38699241cb5be6a9fb3209a2355e8febd
Manuel Moos <z-man@users.sf.net>
2026-09-18 00:01:09 +0200

    Transfer comment from 0.2.9

--- src/tools/tCoord.h
+++ src/tools/tCoord.h
@@ -67,8 +67,7 @@ public:
         return(a.x*b.x+a.y*b.y);
     }
 
-    //! change coordinates, so that a is at zero and c is at one.
-    //! gives the coordinate of b.
+    // V(a, b, c) gives X coordinate of b in an orthogonal coordinate system when a is at (0,0) and c is at (1,0)
     static REAL V(const tCoord &a,const tCoord &b,const tCoord &c){
         tCoord ab=b-a;
         tCoord ac=c-a;
